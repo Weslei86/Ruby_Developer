1e8a404227c9a2a77ad8ad8102d3400dbd298a54
Weslei86 <113482094+Weslei86@users.noreply.github.com>
2024-02-25 16:07:42 -0300

    Desafio1 Ruby
    Código Ruby solicitação de nome, sobrenome e idade do usuário.

---
+++ desafio1.rb
@@ -0,0 +1,9 @@
+print "Bem vindo a Biblioteca \n
+Digite o seu nome: "
+nome = gets.chomp
+print "Digite o seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite a sua idade: "
+idade = gets.chomp
+#chamar os dados #{variáveis}
+puts "#{nome} #{sobrenome} tem #{idade} anos, seja bem vindo(a)!"
